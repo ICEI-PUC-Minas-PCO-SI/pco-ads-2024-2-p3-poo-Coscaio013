880952ce848f83cfa5690f95ef0b0fe82fb0337a
Caio Ferreira Araujo <caiof13araujo@gmail.com>
2024-11-20 18:46:48 -0300

    .

---
+++ atividade CRUD FORMS XML/Form1.cs
@@ -0,0 +1,9 @@
+namespace atividade_CRUD_FORMS_XML;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
--- atividade CRUD FORMS XML/obj/Debug/net8.0-windows/atividade CRUD FORMS XML.AssemblyInfo.cs
+++ atividade CRUD FORMS XML/obj/Debug/net8.0-windows/atividade CRUD FORMS XML.AssemblyInfo.cs
@@ -10,13 +10,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade Herança (Veículos)")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("atividade CRUD FORMS XML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502fa79f3794dd659adb68a11cd972d17de32755")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atividade Herança (Veículos)")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atividade Herança (Veículos)")]
+[assembly: System.Reflection.AssemblyProductAttribute("atividade CRUD FORMS XML")]
+[assembly: System.Reflection.AssemblyTitleAttribute("atividade CRUD FORMS XML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
